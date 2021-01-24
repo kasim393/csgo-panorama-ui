3072e2b3a5a41fdd744a0f04fffeb25a159af746
Kasim Shaikh <46850168+kasim393@users.noreply.github.com>
2021-01-24 19:58:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ function togglemarket(){
   Indicator2.style.transform = "translateX(215px)";
 }
 
+
+//play csgo tab
+
+function playTab(evt, tabname) {
+  var i, tabcontent, tablinks;
+
+  
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  
+  document.getElementById(tabname).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
